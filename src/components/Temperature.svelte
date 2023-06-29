9f8ee181759fcb811b0f31b5fec590677ce8b4e3
Nills <nills.van.limbeek@live.nl>
2023-06-29 21:19:02 +0200

    first commit

---
+++ src/components/Temperature.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	export let height: number;
+	export let topDegrees: number;
+	export let bottomDegrees: number;
+</script>
+
+<div class="temperature" style="height: {height}px;">
+	<div class="inner">
+		<p class="top">{topDegrees}° F</p>
+		<p class="bottom">{bottomDegrees}° F</p>
+	</div>
+</div>
+
+<style lang="scss">
+	.temperature {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		transition: height 0.5s ease-in-out;
+
+		height: 200px;
+		width: 60px;
+		border-radius: 30px;
+		background: linear-gradient(
+			180deg,
+			#ff0000 -24.77%,
+			#f16211 -12.15%,
+			#9747ff 48.57%,
+			#2776e6 87.21%,
+			#25d7fe 112.44%,
+			#ffffff 126.64%
+		);
+	}
+
+	.inner {
+		height: calc(100% - 7.5px);
+		width: calc(100% - 7.5px);
+
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+
+		border-radius: 30px;
+		background-color: #1e1f22;
+
+		& p {
+			text-transform: uppercase;
+			color: white;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import Temperature from '../components/Temperature.svelte';
+
+	let height: number = 200;
+	let topDegrees: number = 90;
+	let bottomDegrees: number = 45;
+</script>
+
+<div class="wrapper">
+	<Temperature {height} {topDegrees} {bottomDegrees} />
+
+	<div class="inputs">
+		<div class="input">
+			<label for="height">Height</label>
+			<input type="number" name="height" bind:value={height} />
+		</div>
+
+		<div class="input">
+			<label for="top-degrees">Top Degrees</label>
+			<input type="number" name="top-degrees" bind:value={topDegrees} />
+		</div>
+
+		<div class="input">
+			<label for="bottom-degrees">Bottom Degrees</label>
+			<input type="number" name="bottom-degrees" bind:value={bottomDegrees} />
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.wrapper {
+		height: 100vh;
+		width: 100vw;
+		background-color: #1e1f22;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.inputs {
+		margin-left: 50px;
+	}
+
+	.input {
+		display: flex;
+		flex-direction: column;
+		margin-bottom: 15px;
+
+		& label {
+			color: white;
+		}
+	}
+</style>
